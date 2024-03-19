a41ab696311b93c5d0a94407fb8bd6304a46e2b2
Sarvjeetjha <jha.sarvjeet1@gmail.com>
2024-03-19 20:28:35 +0530

    bubble sort using recursion

--- 2_Sorting techniques/1_Bubble_sort.cpp
+++ 2_Sorting techniques/1_Bubble_sort.cpp
@@ -1,23 +1,50 @@
 /*Bubble sort*/
-#include<iostream>
-#include <bits/stdc++.h> 
+#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
-void bubbleSort(vector<int>& arr, int n) 
+// using iteratiion
+//  void bubbleSort(vector<int>& arr, int n)
+//  {
+
+//     for(int i=0;i<arr.size();i++){
+//         for(int j=i+1;j<arr.size();j++){
+//             if(arr[j]<arr[i])
+//                 swap(arr[i],arr[j]);
+//         }
+//     }
+// }
+
+void bubbleSort(vector<int>&arr, int n)
 {
-    
-    for(int i=0;i<arr.size();i++){
-        for(int j=i+1;j<arr.size();j++){
-            if(arr[j]<arr[i])
-                swap(arr[i],arr[j]);
+    if (n == 1)
+        return;
+    else
+    {
+        for (int i = 0; i < n - 1; i++)
+        {
+            if (arr[i + 1] < arr[i])
+                swap(arr[i], arr[i + 1]);
+            
         }
+        bubbleSort(arr,n-1);
     }
 }
-int main(){
-vector<int> arr={2,9,0,-8,-6,8,4,3};
-bubbleSort(arr,arr.size());
-for(int i=0;i<arr.size();i++){
-    cout<<arr[i]<<" "; 
+void print(vector<int>arr){
+    for (int i = 0; i < arr.size(); i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout<<endl;
 }
-    
-     return 0;
+int main()
+{
+    vector<int> arr = {2, 9, 0, -8, -6, 8, 4, 3};
+    cout<<"array before sorting"<<endl;
+    print(arr);
+    // bubbleSort(arr, arr.size()); // function call for iterartice approach
+    bubbleSort(arr,arr.size());
+    cout<<"array after sorting"<<endl;
+    print(arr);
+
+    return 0;
 }
