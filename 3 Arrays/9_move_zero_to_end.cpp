9b91d9a4ab8441e52a4930b617a5fb932e257945
Sarvjeetjha <jha.sarvjeet1@gmail.com>
2024-04-07 20:01:38 +0530

    updated with optimal solution

--- 3 Arrays/9_move_zero_to_end.cpp
+++ 3 Arrays/9_move_zero_to_end.cpp
@@ -1,34 +1,84 @@
-#include<iostream>
-#include <bits/stdc++.h> 
+#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
-void moveZeroToEnd(int arr[],int n){
-    int temp[n];
-    int j=0;
-    for(int i=0;i<n;i++){
-        if(arr[i]!=0){
-            temp[j++]=arr[i];
+// Brute force approach
+// void moveZeroToEnd(int arr[],int n){
+//     int temp[n];
+//     int j=0;
+//     for(int i=0;i<n;i++){
+//         if(arr[i]!=0){
+//             temp[j++]=arr[i];
+//         }
+//     }
+//     for(int i=0;i<j;i++){
+//         arr[i]=temp[i];
+
+//     }
+//     while(j<n){
+//         arr[j++]=0;
+//     }
+// }
+// void print(int arr[],int n){
+//     for(int i=0;i<n;i++){
+//         cout<<arr[i]<<" ";
+//     }
+//     cout<<endl;
+// }
+// optimal approach
+// void moveZeroToEnd(int arr[], int n)
+// {
+//     int j = -1;
+//     for (int i = 0; i < n; i++)
+//     {
+//         if (arr[i] == 0)
+//         {
+//             j = i;
+//             break;
+//         }
+//     }
+//     for(int i=j+1;i<n;i++){
+//         if(arr[i]!=0){
+//             swap(arr[i],arr[j]);
+//             j++;
+//         }
+//     }
+
+// }
+//clean code
+void moveZeroToEnd(int arr[], int n)
+{
+    int j = -1;
+    for (int i = 0; i < n; i++)
+    {
+        if (arr[i]!=0)
+        {
+            j++;
+            swap(arr[i],arr[j]);
+
         }
     }
-    for(int i=0;i<j;i++){
-        arr[i]=temp[i];
+    // for(int i=j+1;i<n;i++){
+    //     if(arr[i]!=0){
+    //         swap(arr[i],arr[j]);
+    //         j++;
+    //     }
+    // }
 
-    }
-    while(j<n){
-        arr[j++]=0;
-    }
 }
-void print(int arr[],int n){
-    for(int i=0;i<n;i++){
-        cout<<arr[i]<<" ";
+void print(int arr[], int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
     }
-    cout<<endl;
+    cout << endl;
 }
-int main(){
-    int arr[]={2,0,3,0,5,4,0,6,8,0,0,54,6,0};
-    print(arr,14);
-    moveZeroToEnd(arr,14);
-    print(arr,14);
+int main()
+{
+    int arr[] = {0,0,0,0,1};
+    print(arr, 5);
+    moveZeroToEnd(arr, 5);
+    print(arr, 5);
 
-    
-     return 0;
+    return 0;
 }
