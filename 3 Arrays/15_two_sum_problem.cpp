119125378b417b7084dc6a06f8fadf3be7c96ce9
Sarvjeetjha <jha.sarvjeet1@gmail.com>
2024-04-16 22:33:43 +0530

    added optimal solution

--- 3 Arrays/15_two_sum_problem.cpp
+++ 3 Arrays/15_two_sum_problem.cpp
@@ -2,43 +2,99 @@
 #include <iostream>
 #include <bits/stdc++.h>
 using namespace std;
+// brute force approach
+//  bool twoSumProblem(int arr[], vector<int> &ans, int sum, int n)
+//  {
+//      bool flag = false;
+//      for (int i =0; i < n; i++)
+//      {
+//          for (int j = i + 1; j < n; j++)
+//          {
+//              if ((arr[i] + arr[j]) == sum)
+//              {
+//                  flag = true;
+//                  ans.push_back(i);
+//                  ans.push_back(j);
+//                  break;
+//              }
+//          }
+//          break;
+//      }
+//      return flag;
+//  }
+
+// better approach
+//most optimal 
 bool twoSumProblem(int arr[], vector<int> &ans, int sum, int n)
 {
     bool flag = false;
-    for (int i = 1; i < n; i++)
+    map<int, int> mpp;
+    for (int i = 0; i < n; i++)
     {
-        for (int j = i + 1; j < n; j++)
+        int a = arr[i];
+        int remain = sum - a;
+        if (mpp.find(remain) != mpp.end())
         {
-            if ((arr[i] + arr[j]) == sum)
-            {
-                flag = true;
-                ans.push_back(i);
-                ans.push_back(j);
-                break;
-            }
+            ans.push_back(mpp[remain]);
+            ans.push_back(i);
+            return true;
         }
-        break;
+        mpp[arr[i]] = i;
     }
-    return flag;
+
+    ans.push_back(-1);
+    ans.push_back(-1);
+    return false;
 }
+
+// optimal approach
+// it's only for the Yes or NO
+// bool twoSumProblem(int arr[], int sum, int n)
+// {
+//     int i = 0;
+//     int j = n - 1;
+//     sort(arr, arr + n);
+//     while (i <= j)
+//     {
+//         if ((arr[i] + arr[j]) == sum)
+//         {
+
+//             return true;
+//         }
+//         else if ((arr[i] + arr[j]) < sum)
+//         {
+//             i++;
+//         }
+//         else
+//         {
+//             j--;
+//         }
+//     }
+
+//     return false;
+// }
+
 int main()
 {
-    int arr[] = {2, 6, 5, 8, 11};
-    int target = 14;
+    int arr[] = {2, 6, 5, 8, 10};
+    int target = 13;
     vector<int> ans;
-    bool result=twoSumProblem(arr,ans,target,5);
-    if(result){
-        cout<<"YES"<<endl;
-        for(int i=0;i<ans.size();i++){
-            cout<<ans[i]<<" ";
+    bool result = twoSumProblem(arr,ans ,target, 5);
+    if (result)
+    {
+        cout << "YES" << endl;
+        for (int i = 0; i < ans.size(); i++)
+        {
+            cout << ans[i] << " ";
         }
     }
-    else{
-        cout<<"NO"<<endl;
-        ans.push_back(-1);
-        ans.push_back(-1);
-         for(int i=0;i<ans.size();i++){
-            cout<<ans[i]<<" ";
+    else
+    {
+        cout << "NO" << endl;
+
+        for (int i = 0; i < ans.size(); i++)
+        {
+            cout << ans[i] << " ";
         }
     }
 
