ae183ba9ed0f3053724b92744ae068ffe6ec2d18
Sarvjeetjha <jha.sarvjeet1@gmail.com>
2024-04-19 15:27:51 +0530

    updated with optimal approach

--- 3 Arrays/5__no_of_unique_element_in_array.cpp
+++ 3 Arrays/5__no_of_unique_element_in_array.cpp
@@ -1,24 +1,50 @@
 #include <iostream>
 #include <bits/stdc++.h>
 using namespace std;
-int removeDuplicates(vector<int> &arr, int n)
+// brute force approach
+// int removeDuplicates(vector<int> &arr, int n) // to count no of unique element
+// {
+//     set<int> s;
+
+//     for (int i = 0; i < n; i++)
+//     {
+//         s.insert(arr[i]);
+//     }
+
+
+//     return s.size();
+// }
+//optimal approach
+int removeDuplicates(vector<int> &arr, int n)// to arrange unique element in order till ith index(i+1) is size of array 
 {
-    int l = 1;
-
-    for (int i = 0; i < n - 1; i++)
-    {
-
-        if (arr[i] != arr[i + 1])
-            l++;
+    int i=0;
+    for(int j=0;j<n;j++){
+        if(arr[i]!=arr[j]){
+            i++;
+            arr[i]=arr[j];
+        }
     }
-
-    return l;
+    return i+1;
 }
+// int removeDuplicates(vector<int> &arr, int n)// to count no of unique element
+// {
+//     int l = 1;
+
+//     for (int i = 0; i < n - 1; i++)
+//     {
+
+//         if (arr[i] != arr[i + 1])
+//             l++;
+//     }
+
+//     return l;
+// }
 
 int main()
-{   vector<int>v={1,2,2,2,3};
-int ans=removeDuplicates(v,5);
-cout<<"no of unique element in array "<<ans;
+{
+    vector<int> v = {1, 2, 2, 2, 3};
+    int ans = removeDuplicates(v, 5);
+    cout << "no of unique element in array " << ans;
 
     return 0;
 }
