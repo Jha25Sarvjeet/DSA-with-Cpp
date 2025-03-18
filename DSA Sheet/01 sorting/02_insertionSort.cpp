9e7f4fda25669ed20a8d8bd3f1f678576b87dbde
Jha25Sarvjeet <jha.sarvjeet1@gmail.com>
2025-03-18 23:17:11 +0530

    updated with interative approach(insertion sort)

--- DSA Sheet/01 sorting/02_insertionSort.cpp
+++ DSA Sheet/01 sorting/02_insertionSort.cpp
@@ -1,18 +1,32 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 #include <iostream>
 using namespace std;
-void insertionSort(int arr[], int n){
-    int i=1;
-    while(i<n){
-        int j=i;
-        while(arr[j]<=arr[j-1] && j>0){
-            swap(arr[j],arr[j-1]);
-            j--;
-        }
-        i++;
+// iterative approach
+
+// void insertionSort(int arr[], int n){
+//     int i=1;
+//     while(i<n){
+//         int j=i;
+//         while(arr[j]<=arr[j-1] && j>0){
+//             swap(arr[j],arr[j-1]);
+//             j--;
+//         }
+//         i++;
+//     }
+// }
+void insertionSort(int arr[], int i, int n)
+{
+    if(i==n)return;
+    int j = i;
+    while (arr[j] <= arr[j - 1] && j > 0)
+    {
+        swap(arr[j], arr[j - 1]);
+        j--;
     }
+    insertionSort(arr,i+1,n);
 }
-int main(){
+int main()
+{
     int arr[] = {4, 3, 5, 1, 2, 5, 6};
     int n = sizeof(arr) / sizeof(arr[0]);
     for (int i = 0; i < n; i++)
@@ -20,7 +34,7 @@ int main(){
         cout << arr[i] << " ";
     }
     cout << endl;
-    insertionSort(arr, n);
+    insertionSort(arr, 1,n);
     for (int i = 0; i < n; i++)
     {
         cout << arr[i] << " ";
