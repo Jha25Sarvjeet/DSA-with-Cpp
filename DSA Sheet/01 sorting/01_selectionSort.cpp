1982415626992b8fd4dc68534bab72222ec44014
Jha25Sarvjeet <jha.sarvjeet1@gmail.com>
2025-03-18 23:01:06 +0530

    updated with recursive approach

--- DSA Sheet/01 sorting/01_selectionSort.cpp
+++ DSA Sheet/01 sorting/01_selectionSort.cpp
@@ -1,32 +1,57 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 #include <iostream>
 using namespace std;
-void selectionSort(int arr[],int n){
-    int j=0;
-    int mini=0;
-    while(j<n-1){
-      
-        for(int i=j;i<n;i++){
-            if(arr[i]<arr[mini]){
-                mini=i;
+// iterative approach
+
+// void selectionSort(int arr[],int n){
+//     int j=0;
+//     int mini=0;
+//     while(j<n-1){
+
+//         for(int i=j;i<n;i++){
+//             if(arr[i]<arr[mini]){
+//                 mini=i;
+//             }
+//         }
+//         swap(arr[j++],arr[mini]);
+//         mini=j;
+//     }
+
+// }
+
+// recursive approach
+void selectionSort(int arr[], int j, int n)
+{
+    if (n == j)
+        return;
+    else
+    {
+        int mini = j;
+
+        for (int i = j; i < n; i++)
+        {
+            if (arr[i] < arr[mini])
+            {
+                mini = i;
             }
         }
-        swap(arr[j++],arr[mini]);
-        mini=j;
+        swap(arr[j], arr[mini]);
+        selectionSort(arr, j + 1, n);
     }
-   
 }
-int main(){
-    int arr[]={4,3,5,1,2,5,6};
-    int n=sizeof(arr)/sizeof(arr[0]);
-    for (int i=0;i<n;i++){
-        cout<<arr[i]<<" ";
+int main()
+{
+    int arr[] = {4, 3, 5, 1, 2, 5, 6};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
     }
-    cout<<endl;
-    selectionSort(arr,n);
-    for (int i=0;i<n;i++){
-        cout<<arr[i]<<" ";
+    cout << endl;
+    selectionSort(arr, 0, n);
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
     }
-    cout<<endl;
-    cout<<endl;
+    cout << endl;
 }
