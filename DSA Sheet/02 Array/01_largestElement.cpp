f371797cf8739602c653a5a5bb22a14732d94563
Jha25Sarvjeet <jha.sarvjeet1@gmail.com>
2025-03-20 15:37:05 +0530

    Largest element (recursive)

--- DSA Sheet/02 Array/01_largestElement.cpp
+++ DSA Sheet/02 Array/01_largestElement.cpp
@@ -1,19 +1,28 @@
 #include <bits/stdc++.h>
 #include <iostream>
 using namespace std;
-int largestElement(int arr[], int n)
-{
-    int maxi = INT_MIN;
-    for (int i = 0; i < n; i++)
-    {
-        if(arr[i]>maxi)maxi=arr[i];
-    }
-    return maxi;
+// int largestElement(int arr[], int n)
+// {
+//     int maxi = INT_MIN;
+//     for (int i = 0; i < n; i++)
+//     {
+//         if(arr[i]>maxi)maxi=arr[i];
+//     }
+//     return maxi;
+// }
+
+
+//recursive approach
+
+int largestElement(int arr[], int j,int n,int maxi)
+{   if(j>=n)return maxi;
+    maxi= max(maxi,arr[j]);
+    largestElement(arr,j+1,n,maxi);
 }
 int main()
 {
     int arr[]={1,8,7,56,90};
     int n=sizeof(arr)/sizeof(arr[0]);
-    int maxi=largestElement(arr,n);
+    int maxi=largestElement(arr,0,n,INT_MIN);
     cout<<"largest element is "<<maxi<<endl;
 }
