1a18f1d553d61fb38b57535eadf0f6b8355c06f8
Jha25Sarvjeet <jha.sarvjeet1@gmail.com>
2025-03-20 16:05:41 +0530

    updated with better approach

--- DSA Sheet/02 Array/02_secondLargest.cpp
+++ DSA Sheet/02 Array/02_secondLargest.cpp
@@ -1,15 +1,36 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 #include <iostream>
 using namespace std;
-//brutre force
+// brutre force
 
-int secondLargest(int arr[],int n){
-    sort(arr,arr+n);
-    return arr[n-2];
+// int secondLargest(int arr[],int n){
+//     sort(arr,arr+n);
+//     return arr[n-2];
+// }
+
+// Better approach
+int secondLargest(int arr[], int n)
+{
+    int largest=INT_MIN;
+    int sLargest=INT_MIN;
+    int slargeIndex=-1;
+    for(int i=0;i<n;i++){
+        if(arr[i]>largest){
+            sLargest=largest;
+            largest=arr[i];
+        }
+        if(arr[i]>sLargest && arr[i]<largest){
+            sLargest=arr[i];
+        }
+    
+    }
+   
+    return  sLargest;
 }
-int main(){
-    int arr[]={5,4,8,6,7,12,15,14};
-    int n=sizeof(arr)/sizeof(arr[0]);
-    int sLargest=secondLargest(arr,n);
-    cout<<"second largest element is : "<<sLargest<<endl;
+int main()
+{
+    int arr[] = {5, 4, 13,8, 6, 7, 12, 15,14};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    int sLargest = secondLargest(arr, n);
+    cout << "second largest element is : " << sLargest << endl;
 }
