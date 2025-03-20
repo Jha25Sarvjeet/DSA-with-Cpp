b66f0f228c6c0119190d65376308b70260f605a9
Jha25Sarvjeet <jha.sarvjeet1@gmail.com>
2025-03-20 16:23:34 +0530

    upadte with better approach

--- DSA Sheet/02 Array/03_checkIfArrayIsSorted.cpp
+++ DSA Sheet/02 Array/03_checkIfArrayIsSorted.cpp
@@ -1,19 +1,31 @@
 #include<bits/stdc++.h>
 #include <iostream>
 using namespace std;
+//brute force approach
+
+// bool checkSorted(int arr[],int n){
+//     int temp[n];
+//     copy(arr,arr+n,temp);//copyin array to another 
+//     sort(arr,arr+n);
+//     for (int i=0;i<n;i++){
+//         if(arr[i]!=temp[i]){
+//             return false;
+//         }
+//     }
+//     return true;
+// }
+//optimal approach
+
 bool checkSorted(int arr[],int n){
-    int temp[n];
-    copy(arr,arr+n,temp);//copyin array to another 
-    sort(arr,arr+n);
-    for (int i=0;i<n;i++){
-        if(arr[i]!=temp[i]){
+    for (int i=1;i<n;i++){
+        if(arr[i]<arr[i-1]){
             return false;
         }
     }
     return true;
 }
 int main(){
-    int arr[] = { 4,8,  12, 11,12,15};
+    int arr[] = { 4,8,  12,12,15};
     int n = sizeof(arr) / sizeof(arr[0]);
     bool sortOrNot=checkSorted(arr,n);
     if(sortOrNot)cout<<"sorted"<<endl;
